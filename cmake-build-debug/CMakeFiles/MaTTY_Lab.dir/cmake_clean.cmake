c966e4a793107ad84ade8e8b47fe580aec373354
Unknown <samuelsoutullosobral@gmail.com>
2017-11-14 11:46:26 +0100

    Project created

---
+++ cmake-build-debug/CMakeFiles/MaTTY_Lab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MaTTY_Lab.dir/main.c.o"
+  "MaTTY_Lab.pdb"
+  "MaTTY_Lab"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MaTTY_Lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
